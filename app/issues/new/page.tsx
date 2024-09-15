0970b26f340d34049e65c3f8662c3da3404d556b
Walid Hamdi <walidhamdidev@gmail.com>
2024-09-15 09:53:24 +0100

    Handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,28 +1,58 @@
 "use client";
-import { Box, Button, Flex, TextField } from "@radix-ui/themes";
+import { Box, Button, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
 import SimpleMdeReact from "react-simplemde-editor";
 
-const page = () => {
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
+const NewIssuePage = () => {
+  const router = useRouter();
+
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <Flex direction="column" gap="3">
+    <form
+      className="flex flex-col gap-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <Box maxWidth={{ sm: "100%", md: "50%" }}>
-        <TextField.Root size="3" placeholder="Enter the issue title" />
+        <TextField.Root
+          size="3"
+          placeholder="Enter the issue title"
+          {...register("title")}
+        />
       </Box>
       <Box maxWidth={{ sm: "100%", md: "50%" }}>
-        <SimpleMdeReact
-          options={{
-            autofocus: true,
-            spellChecker: false,
-            placeholder: "Write your markdown...",
-          }}
-        />
+        {
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <SimpleMdeReact
+                options={{
+                  autofocus: true,
+                  spellChecker: false,
+                  placeholder: "Write your markdown...",
+                }}
+                {...field}
+              />
+            )}
+            name="description"
+          />
+        }
       </Box>
       <Box maxWidth={{ sm: "100%", md: "25%" }}>
         <Button size="3">Submit New Issue</Button>
       </Box>
-    </Flex>
+    </form>
   );
 };
 
-export default page;
+export default NewIssuePage;
