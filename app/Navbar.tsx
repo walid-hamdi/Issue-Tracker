bc0ea47f5c6dde486dc4abc5719aa3e722591b12
Walid Hamdi <walidhamdidev@gmail.com>
2024-09-18 21:08:44 +0100

    Add login and logout buttons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { Box, Button } from "@radix-ui/themes";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const Navbar = () => {
   const pathName = usePathname();
+  const { status, data } = useSession();
+  const router = useRouter();
 
   const links = [
     { label: "Dashboard", href: "/" },
@@ -18,19 +22,33 @@ const Navbar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link
-            key={link.label}
-            href={link.href}
-            className={classNames({
-              "text-zinc-900": link.href === pathName,
-              "text-zinc-500": link.href !== pathName,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === pathName,
+                "text-zinc-500": link.href !== pathName,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        <Button
+          className="cursor-pointer"
+          onClick={() => {
+            if (status === "authenticated") router.push("/api/auth/signout");
+            else if (status === "unauthenticated")
+              router.push("/api/auth/signin");
+          }}
+        >
+          {status === "authenticated" && "Logout"}
+          {status === "unauthenticated" && "Login"}
+        </Button>
+      </Box>
     </nav>
   );
 };
