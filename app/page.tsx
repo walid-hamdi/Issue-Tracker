6213a00d00fe412c1ea55cdbc596875996fb8f60
Walid Hamdi <walidhamdidev@gmail.com>
2024-09-20 21:25:10 +0100

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <h1>Issue Tracker</h1>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
 }
